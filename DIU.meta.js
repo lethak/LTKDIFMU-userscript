a66f969ff82bacc3d4efa78addf60dbb5cd19fe9
LethaK Maas <lethak@users.noreply.github.com>
2016-01-05 14:01:00 +0100

    Update DIU.meta.js

--- DIU.meta.js
+++ DIU.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Digitally Imported Userscript
 // @namespace   LTKDIFMU
-// @version     2016.1.5b
+// @version     2016.1.6
 // @author      LethaK Maas
 // @description Removes afk popup and minimize ads if possible
 // @include     http://www.di.fm*
