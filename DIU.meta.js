9f3839bf0f8b2d58822910d2ced49b2c047c9416
LethaK Maas <github@souverain.org>
2017-03-19 18:15:51 +0100

    +.modal-btn.continue click

--- DIU.meta.js
+++ DIU.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Digitally Imported Userscript
 // @namespace   LTKDIFMU
-// @version     2017.3.19
+// @version     2017.3.20
 // @author      LethaK Maas
 // @description Removes afk popup and minimize ads if possible
 // @include     http://www.di.fm*
