7773b5a407ed70c562b5cf496b20c82097d1a403
LethaK Maas <lethak@users.noreply.github.com>
2017-08-22 17:41:36 +0200

    di.fm now forcing https

--- DIU.meta.js
+++ DIU.meta.js
@@ -6,6 +6,8 @@
 // @description Removes afk popup and minimize ads if possible
 // @include     http://www.di.fm*
 // @include     http://www.di.fm/*
+// @include     https://www.di.fm*
+// @include     https://www.di.fm/*
 // @downloadURL https://github.com/lethak/digitally_imported_userscript/raw/master/DIU.user.js
 // @updateURL   https://github.com/lethak/digitally_imported_userscript/raw/master/DIU.meta.js
 // @icon        http://i.imgur.com/7yj2PGY.png
