cf25830e22939d109460294ccb7fde882bb46446
LethaK Maas <lethak@users.noreply.github.com>
2017-08-22 17:42:04 +0200

    di.fm now forcing https

--- DIU.meta.js
+++ DIU.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Digitally Imported Userscript
 // @namespace   LTKDIFMU
-// @version     2017.4.0
+// @version     2017.8.0
 // @author      LethaK Maas
 // @description Removes afk popup and minimize ads if possible
 // @include     http://www.di.fm*
